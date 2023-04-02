0008202ba57f07d445a9860a3f98be7a24c003eb
miszyman <mszymanowski97@gmail.com>
2023-04-02 16:15:43 +0200

    Added a check for user input

--- dice_roll_sim.py
+++ dice_roll_sim.py
@@ -10,3 +10,34 @@ def dice_roll(dice_size=6):
     return random.randint(1, dice_size)
 
 
+def user_input():
+    """
+    Prompts used to input string data defining parameters of dice rolls.
+    Takes string in format: xDy+z
+    where:
+    x - number of dice (optional, default = 1)
+    y - size of the single dice from set [3, 4, 6, 8, 10, 12, 20, 100]
+    z - modifier (optional, default = 0)
+    :return: tuple of int values (number, dice_size, modifier)
+    """
+    while True:
+        input_str = input("Enter a dice you'd like to roll in format xDy+z: ").lower()
+
+        # Test for empty input
+        if not input_str:
+            print('Invalid input.')
+            continue
+
+        # Test for 'd'
+        if 'd' not in input_str:
+            print('Invalid input.')
+            continue
+
+        
+
+
+
+
+
+
+user_input()
